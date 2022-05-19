9db1cbd9a5b0cb43f2b2ccc0720c17e99c564a11
wkqkel <89131236+wkqkel@users.noreply.github.com>
2022-05-20 01:17:31 +0900

    fix: 요청 에러 처리

--- src/services/disease.ts
+++ src/services/disease.ts
@@ -2,23 +2,22 @@ import axios from 'axios';
 
 import { IDiseaseAPIRes } from 'types/disease.d';
 
-const PROXY = window.location.hostname === 'localhost' ? '' : '/cors-proxy/';
-const BASE_URL = '/B551182/diseaseInfoService/getDissNameCodeList';
+const BASE_URL = `https://myproxyserv.herokuapp.com/https://apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList`;
 
 interface Params {
   searchText: string;
 }
 
 export const getDiseaseNameApi = (params: Params) =>
-  axios.get<IDiseaseAPIRes>(`${PROXY}${BASE_URL}`, {
+  axios.get<IDiseaseAPIRes>(`${BASE_URL}`, {
     params: {
       serviceKey: process.env.REACT_APP_API_KEY,
       pageNo: 1,
       numOfRows: 10,
       sickType: 1,
       medTp: 2,
-      diseaseType: 'SICK_NM',
       _type: 'json',
+      diseaseType: 'SICK_NM',
       ...params,
     },
   });
