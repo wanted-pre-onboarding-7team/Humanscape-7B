65623c425f903d0aff4ffc0df1a1b87b1e72afd8
Leejin-Yang <ejin1996@gmail.com>
2022-05-20 22:30:15 +0900

    refactor: proxy 수정

--- src/setupProxy.ts
+++
@@ -1,8 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export const apiProxy = createProxyMiddleware('/api', {
-  target: 'http://apis.data.go.kr',
-  changeOrigin: true,
-  router: { '/api': 'http://apis.data.go.kr' },
-  pathRewrite: { '^/api': '' },
-});
